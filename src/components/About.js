55d4f300e0afd31846a2282213eb97d50485ac65
Dennis Shakava <dennis.shakava@student.moringaschool.com>
2022-07-13 20:07:33 +0300

    updated details.

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { image } from "../data/data";
 
+// let png = "image"
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+  <h2>About Me</h2>
+  <p>I love React framework expedites my work</p>
+  <img src={image} alt="I made this"/>
+  
+  </div>;
 }
 
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import { name, city } from "../data/data.js";
+import { name1, city1 } from "../data/data";
 
 function App() {
   return (

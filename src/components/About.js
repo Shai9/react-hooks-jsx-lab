74b28c2ce750b2a01335938ce286c6f6fee920e6
Dennis Shakava <dennis.shakava@student.moringaschool.com>
2022-07-13 20:12:21 +0300

    changes on about.

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ import { image } from "../data/data";
 function About() {
   return <div id="about">
   <h2>About Me</h2>
-  <p>I love React framework since it expedites my work</p>
+  <p>I love React ; Learn Once / Write Anywhere</p>
   <img src={image} alt="I made this"/>
   
   </div>;

5e07a34376da7cb83ae8773636a2aa0e78e59d60
Dennis Shakava <dennis.shakava@student.moringaschool.com>
2022-07-13 20:28:15 +0300

    updated.

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
   // update the JSX being returned!
   return <div id = "home" >
   <h1 style= {{color: "firebrick"}}> 
-  {name} is a Web Developer from {city}</h1>
+  {name} is a Web Developer from {city}</h1>;
   
   </div>;
 }
